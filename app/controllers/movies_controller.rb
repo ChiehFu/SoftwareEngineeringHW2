f8c6c010f96f74bd38a848edf8a61f51f3dc9a23
EC2 Default User <ec2-user@ip-172-31-13-6.us-east-2.compute.internal>
2018-09-27 02:13:43 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,9 +11,9 @@ class MoviesController < ApplicationController
   end
   
   def index
+    
     @all_ratings = Movie.distinct.pluck(:rating)
     @sort_method = [:title, :release_date]
-    
     if session[:second].nil?
       session[:second] = true
       #initialize session
@@ -26,47 +26,63 @@ class MoviesController < ApplicationController
       end
     end
     
+    
     #get params and update session
-    hash = params["ratings"]
-    if (!hash.nil?) 
-      array = hash.keys
+    rank_hash = params["ratings"]
+    if (!rank_hash.nil?)   
+      key_hash = rank_hash.keys
+      @checked_item = []
       @all_ratings.each do |rank|
-        if (array.include?(rank))
+        if (key_hash.include?(rank))
           session[rank.to_sym] = true
+          @checked_item.push(rank.to_sym)
         else
           session[rank.to_sym] = false
         end
       end
-    end
-    
-    #update session for sorting methods
-    if request.path == '/movies/title'
-      session[:title] = !(session[:title])
-      session[:release_date] = false
-    elsif request.path == '/movies/release'
-      session[:title] = false
-      session[:release_date] = !session[:release_date]
-    end
-    
-    #get checked item from session
-    @checked_item = []
-    @all_ratings.each do |rank|
-      if session[rank.to_sym] == true
-        @checked_item.push(rank)
+      if !params["commit"].nil? && params["commit"] = "Refresh"
+        flash.keep
+        redirect_to movies_path
       end
-    end
-    
-    #get sorting method from session
-    @method = []
-    @sort_method.each do |m|
-      if session[m.to_sym] == true
-        @method.push(m)
+      @method = []
+      if (!params["title"].nil?)
+        @method = [:title]
+      elsif (!params["release_date"].nil?)
+        @method = [:release_date]
       end
+      @movies = Movie.where(rating: @checked_item).order(@method)
+    else
+      redir_add = "?"
+      @all_ratings.each do |rank|
+        if (session[rank.to_sym] == true)
+          redir_add += "&ratings[#{rank}]=true"
+        end
+      end
+      
+      if !params["title"].nil?
+        if session[:title] == true
+          session[:title] = false
+        else
+          session[:title] = true
+          session[:release_date] = false
+        end
+      elsif !params["release_date"].nil?
+        if session[:release_date] == true
+          session[:release_date] = false
+        else
+          session[:release_date] = true
+          session[:title] = false
+        end
+      end
+      
+      if session[:title] == true
+        redir_add += "&title=true"
+      elsif session[:release_date] == true
+        redir_add += "&release_date=true"
+      end
+      flash.keep
+      redirect_to movies_path + URI.encode(redir_add)
     end
-    
-    @movies = Movie.where(rating: @checked_item).order(@method)
-    @path1 = '/movies/title'
-    @path2 = '/movies/release'
   end
   
   def new
